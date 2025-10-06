173575e6fdd5f66d0c735d2a2b9b3b4468716eb3
Danielamezag <mezadaniela713@gmail.com>
2025-10-06 09:36:00 -0400

    first commit

---
+++ sql_load/INSERT INTO company_dim (company_id, nam.sql
@@ -0,0 +1,8 @@
+INSERT INTO company_dim (company_id, name, link, link_google, thumbnail)
+VALUES (
+    company_id:integer,
+    'name:text',
+    'link:text',
+    'link_google:text',
+    'thumbnail:text'
+  );
